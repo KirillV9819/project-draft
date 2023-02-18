275cac841f8c434a6d21baeb043d06ea46a7f161
KirillV9819 <kirillv9819@gmail.com>
2023-02-18 19:54:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,5 @@
-const atTheOldToad = {
-  potions: [
-    { name: "Speed potion", price: 460 },
-    { name: "Dragon breath", price: 780 },
-    { name: "Stone skin", price: 520 },
-    ],
-  // Change code below this line
-  getPotions() {
-    return this.potions;
-  },
-    addPotion( newPotion ) {
-        for (let potion of this.potions) {
-            
-            if (potion.name === newPotion.name) {
-               return `Error! Potion ${newPotion.name} is already in your inventory!`;
-            }    
-        
-        }
-        this.potions.push(newPotion);
-        console .table(this.potions)
-    },
-    
-//   removePotion(potionName) {
-//     // const potionIndex = this.potions.indexOf(potionName);
+function initSnake(str) {
+  return str.toLowerCase().replaceAll(" ", "_")
+};
 
-//     // if (potionIndex === -1) {
-//     //   return `Potion ${potionName} is not in inventory!`;
-//     // }
-
-//     // this.potions.splice(potionIndex, 1);
-//   },
-//   updatePotionName(oldName, newName) {
-//     // const potionIndex = this.potions.indexOf(oldName);
-
-//     // if (potionIndex === -1) {
-//     //   return `Potion ${oldName} is not in inventory!`;
-//     // }
-
-//     // this.potions.splice(potionIndex, 1, newName);
-//   },
-    };
-
-console. log (atTheOldToad.addPotion({ name: "Dragon ", price: 780}))
+console.log(initSnake("Javascript is awesome"))
